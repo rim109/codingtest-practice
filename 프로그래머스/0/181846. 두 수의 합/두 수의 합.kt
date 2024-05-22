f924bd72ae7f70fe8275bd3f21c26b97ef442cd3
rim109 <132591560+rim109@users.noreply.github.com>
2024-05-22 13:15:02 +0900

    [level 0] Title: 두 수의 합, Time: 227.11 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(a: String, b: String): String {
+        val aa = a.toBigInteger()
+        val bb = b.toBigInteger()
+        val sum = aa + bb
+        
+        return sum.toString()
+    }
+}
