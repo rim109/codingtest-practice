a314ee8eb7fe961c290f71baaef14bf57dd28532
rim109 <132591560+rim109@users.noreply.github.com>
2024-06-08 11:12:01 +0900

    [level 0] Title: 외계행성의 나이, Time: 12.65 ms, Memory: 61.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(age: Int): String {
+        var answer = age.toString()
+        return answer.map{
+            when(it){
+                '0' -> "a"
+                '1' -> "b"
+                '2' -> "c"
+                '3' -> "d"
+                '4' -> "e"
+                '5' -> "f"
+                '6' -> "g"
+                '7' -> "h"
+                '8' -> "i"
+                '9' -> "j"
+                else -> ""
+            }          
+        }.joinToString("")
+    }
+}
