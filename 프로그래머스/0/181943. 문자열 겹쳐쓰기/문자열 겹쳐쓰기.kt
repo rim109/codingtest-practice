668475c3097233e0ad2be5ffb33a1deb9066dce3
rim109 <132591560+rim109@users.noreply.github.com>
2024-06-13 00:13:31 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 22.07 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
@@ -0,0 +1,4 @@
+class Solution {
+    fun solution(my_string: String, overwrite_string: String, s: Int) =
+    my_string.substring(0, s) + overwrite_string + my_string.substring(s + overwrite_string.length until my_string.length)
+}
