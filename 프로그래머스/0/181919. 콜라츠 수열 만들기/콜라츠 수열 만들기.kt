ca4aeb962c7e54fcd6adca5818333189cec1f784
rim109 <132591560+rim109@users.noreply.github.com>
2024-09-30 21:24:03 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 8.55 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.kt
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.kt
@@ -1,9 +1,8 @@
 class Solution {
     fun solution(n: Int): IntArray {
-        val answer = mutableListOf<Int>()
+        val answer = mutableListOf<Int>().apply { add(n) }
         var num = n
 
-        answer.add(num)
         while (num > 1) {
             if (num % 2 == 0) {
                 num /= 2
