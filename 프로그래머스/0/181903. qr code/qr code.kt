76692568f887b400a47b629b8008ce64dcc5937a
rim109 <132591560+rim109@users.noreply.github.com>
2024-11-19 20:05:25 +0900

    [level 0] Title: qr code, Time: 25.89 ms, Memory: 59 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(q: Int, r: Int, code: String): String {
+        var answer: String = ""
+        for(i in 0 until code.length){
+            if(i % q == r){
+                answer += code[i]
+            }
+        }
+        return answer
+    }
+}
