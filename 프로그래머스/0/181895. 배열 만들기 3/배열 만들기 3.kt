4a0294517ab635f45e73c79bba82845860e08a5e
rim109 <132591560+rim109@users.noreply.github.com>
2024-11-21 20:51:25 +0900

    [level 0] Title: 배열 만들기 3, Time: 16.55 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.kt
@@ -0,0 +1,4 @@
+class Solution {
+    fun solution(arr: IntArray, intervals: Array<IntArray>): IntArray = 
+    arr.sliceArray(intervals[0][0] .. intervals[0][1]) + arr.sliceArray(intervals[1][0] .. intervals[1][1])
+    }
