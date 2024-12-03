852de7dad5dd3d83837122e5af785ce5b6b9a42b
rim109 <132591560+rim109@users.noreply.github.com>
2024-12-03 19:50:52 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.06 ms, Memory: 63.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(board: Array<IntArray>, k: Int): Int {
+        var answer: Int = 0
+        for (i in board.indices){
+            for (j in board[i].indices){
+                if (i + j <= k){
+                    answer += board[i][j]
+                }
+            }
+        }
+        return answer
+    }
+}
