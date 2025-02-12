9ca10bc720f6a94db61c51523affcb06bb9b6138
rim109 <132591560+rim109@users.noreply.github.com>
2025-02-12 18:35:22 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String n_str) {
+        int answer = Integer.parseInt(n_str);
+        String result = Integer.toString(answer);
+        return result;
+    }
+}
