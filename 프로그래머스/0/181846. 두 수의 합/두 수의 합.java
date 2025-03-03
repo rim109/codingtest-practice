17c1def37c91c42bd792ba3243a33503835f2498
rim109 <132591560+rim109@users.noreply.github.com>
2025-03-03 10:01:57 +0900

    [level 0] Title: 두 수의 합, Time: 407.08 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String solution(String a, String b) {
+        BigInteger aa = new BigInteger(a);
+        BigInteger bb = new BigInteger(b);
+        BigInteger answer = aa.add(bb);    
+        return answer.toString();
+    }
+}
