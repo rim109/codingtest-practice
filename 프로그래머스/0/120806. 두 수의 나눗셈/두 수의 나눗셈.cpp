67c61cce6453f7e852854d03b99e133eabdeacf4
rim109 <132591560+rim109@users.noreply.github.com>
2026-09-11 23:15:04 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.94 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -4,6 +4,6 @@
 using namespace std;
 
 int solution(int num1, int num2) {
-    double answer = (static_cast<double>(num1) / num2)*1000;
+    double answer = (double(num1) / num2) *1000;
     return answer;
 }
