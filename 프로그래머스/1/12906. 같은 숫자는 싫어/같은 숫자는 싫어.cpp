3c684fae42c7db4091a235c78fa55c98d355e96e
rim109 <132591560+rim109@users.noreply.github.com>
2026-09-12 22:46:02 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 97.72 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    for (int i = 0; i < arr.size(); i++){
+        if(i == 0 || arr[i] != arr[i-1]){
+            answer.push_back(arr[i]);
+        }
+    }
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
