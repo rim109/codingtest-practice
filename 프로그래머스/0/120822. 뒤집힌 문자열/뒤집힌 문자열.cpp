0f63dbf95f3a46d276c6958e451c5238dfa7ee6f
rim109 <132591560+rim109@users.noreply.github.com>
2026-09-14 22:01:51 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.00 ms, Memory: 4.88 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cpp
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cpp
@@ -8,3 +8,6 @@ string solution(string my_string) {
     reverse(my_string.begin(), my_string.end());
     return my_string;
 }
+
+
+  //  return string(my_string.rbegin(), my_string.rend()); 이러한 방법도 있음
